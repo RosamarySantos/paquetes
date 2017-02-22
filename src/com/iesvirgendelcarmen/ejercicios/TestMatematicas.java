6121995651958d939e8c7a09e7023db209adc0b2
Manuel Molino <manolo@iesvirgendelcarmen.com>
2017-02-22 14:27:36 +0100

    commit para Ángel

--- src/com/iesvirgendelcarmen/ejercicios/TestMatematicas.java
+++ src/com/iesvirgendelcarmen/ejercicios/TestMatematicas.java
@@ -1,10 +1,18 @@
 package com.iesvirgendelcarmen.ejercicios;
 
-import com.iesvirgendelcarmen.ejercicios.matematicas.Geometria;
+import java.util.Arrays;
+import java.util.List;
+
+//Las dos formas de abajo son similares
+//import com.iesvirgendelcarmen.ejercicios.matematicas.Estadistica;
+//import com.iesvirgendelcarmen.ejercicios.matematicas.Geometria;
+import com.iesvirgendelcarmen.ejercicios.matematicas.*;
+
 
 public class TestMatematicas {
 
 	public static void main(String[] args) {
+		//comprobamos la clase com.iesvirgendelcarmen.ejercicios.matematicas.Geometria
 		double altura = 2.3;
 		double base = 1.6;
 		System.out.printf("Area de un triangulo de base %.2f y altura %.2f"
@@ -17,6 +25,22 @@ public class TestMatematicas {
 				lado, Geometria.calcularAreaPentagono(lado));
 		System.out.printf("Area de un hexágono de lado %.2f vale %.2f%n",
 				lado, Geometria.calcularAreaHexagono(lado));
+		//comprobamos la clase com.iesvirgendelcarmen.ejercicios.matematicas.Estadistica
+		Double[] listaNumeros = {122.0, 122.0, 123.3, 1111.0, 1.3, 1.3, 1.1, 1.2, 1.1, 1.2, 1.0, 1.3};
+		List<Double> listaNumerosDinamica = Arrays.asList(listaNumeros);
+		Estadistica e = new Estadistica(listaNumerosDinamica);
+		System.out.println("La lista de número es: " + e);
+		System.out.printf("El valor medio de la lista es %.2f%n",
+				e.calcularValorMedio());
+		System.out.printf("La varianza de la lista es %.2f%n",
+				e.calcularVarianza());
+		System.out.printf("La desviación típica de la lista es %.2f%n",
+				e.calcularDesviacionTipica());
+		System.out.printf("La moda de la lista es %s%n",
+				e.calcularModa());
+		
+		
+		
 	}
 
 }
--- src/com/iesvirgendelcarmen/ejercicios/matematicas/Estadistica.java
+++ src/com/iesvirgendelcarmen/ejercicios/matematicas/Estadistica.java
@@ -1,17 +1,24 @@
 package com.iesvirgendelcarmen.ejercicios.matematicas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Estadistica {
 	//atributo: colección de doubles
-	List<Double> listaNumeros = new ArrayList<>();
+	List<Double> listaNumeros;
 	//constructor para el caso que le pasamos la lista de doubles
 
 	public Estadistica(List<Double> listaNumeros) {
 		this.listaNumeros = listaNumeros;
+	}
+	public Estadistica(){
+		this.listaNumeros = new ArrayList<>();
 	}
 	//en el caso que no pasemo una lista de doubles, debemos añadir 
 	//los número de uno en uno
@@ -43,14 +50,47 @@ public class Estadistica {
 		return Math.sqrt(calcularVarianza());
 	}
 	//calculo de la moda
-	public List<Double> calcularModa(){
-		List<Double> moda = null;
-		//código:
-		//crear un hashmap (clave,valor) (numero, veces que se repite)
-		//creamos el diccionario vació
-		Map<Double, Integer> diccionario = new HashMap<Double, Integer>();
-		return moda;
+	public Set<Double> calcularModa(){
+		Set<Double> conjuntoModa = new HashSet<Double>();
+		int frecuenciaMaxima = Collections.frequency(listaNumeros, 0);
+		for(Double i : listaNumeros){
+			int frecuenciaDei =  Collections.frequency(listaNumeros, i);
+			if (frecuenciaDei == frecuenciaMaxima )
+				conjuntoModa.add(i);
+			else if (frecuenciaDei > frecuenciaMaxima) {
+				conjuntoModa.clear();
+				frecuenciaMaxima = Collections.frequency(listaNumeros, i);
+				conjuntoModa.add(i);
+			}
+		}
+		return conjuntoModa;
 	}
+	/*public static void main(String[] arg){
+		Integer[]  listaArray = {0,2,33,1,3,4,5};
+		List<Integer> listaDinamica = Arrays.asList(listaArray);
+		System.out.println("ARRAY: " + Arrays.toString(listaArray));
+		System.out.println("LISTA: " + listaDinamica);
+		//creamos un conjunto
+		Set<Integer> conjunto = new HashSet<Integer>();
+		for(Integer i : listaDinamica){
+			//System.out.println(i);
+			conjunto.add(i);
+		}
+		Set<Integer> conjuntoModa = new HashSet<Integer>();
+		int frecuenciaMaxima = Collections.frequency(listaDinamica, 0);
+		
+		System.out.println("CONJUNTO: " + conjunto);
+		for(Integer i : conjunto){
+			if ( Collections.frequency(listaDinamica, i) == frecuenciaMaxima )
+				conjuntoModa.add(i);
+			else if (Collections.frequency(listaDinamica, i) > frecuenciaMaxima) {
+				conjuntoModa.clear();
+				frecuenciaMaxima = Collections.frequency(listaDinamica, i);
+				conjuntoModa.add(i);
+			}
+		}
+		System.out.println("CONJUNTO MODA: " + conjuntoModa);
+	}*/
 	
 	
 	
